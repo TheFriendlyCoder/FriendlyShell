a92a479bc95598dec6402560c6af85aebf0ee7e5
Kevin Phillips <thefriendlycoder@gmail.com>
2017-11-10 20:32:14 -0400

    added readme and travisci config

---
+++ README.rst
@@ -0,0 +1,11 @@
+.. This is a readme file encoded in reStructuredText format, intended for use on the summary page for the pyjen
+.. PyPI project. Care should be taken to make sure the encoding is compatible with PyPI's markup
+.. syntax. See this site for details:
+.. http://docutils.sourceforge.net/docs/ref/rst/restructuredtext.html
+..
+
+=============
+Overview
+=============
+
+Framework for writing interactive Python command line interfaces, similar to the 'cmd' build in class.
--- setup.py
+++ setup.py
@@ -1,5 +1,52 @@
 #!/usr/bin/env python
+import os
 from setuptools import setup, find_packages
+
+
+def check_tag_name(tag_name):
+    """Ensures the name of the current SCM tag is correctly formatted
+
+    Tag should represent a version number of the form X.Y.Z
+
+    :returns: True if the tag name satisfies the expected format, false if not
+    """
+
+    parts = tag_name.split(".")
+    if len(parts) != 3:
+        return False
+
+    for cur_digit in parts:
+        if not cur_digit.isdigit():
+            return False
+
+    return True
+
+
+def get_version_number():
+    """Retrieves the version number for a project"""
+
+    # If we are building from a tag using Travis-CI, set our version number to the tag name
+    if 'TRAVIS_TAG' in os.environ and not os.environ['TRAVIS_TAG'] == '':
+        if not check_tag_name(os.environ['TRAVIS_TAG']):
+            raise Exception("Invalid tag name {0}. Must be of the form 'X.Y.Z'".format(os.environ['TRAVIS_TAG']))
+        return os.environ['TRAVIS_TAG']
+
+    # if we get here we know we're building a pre-release version
+    # so we set a fake version as a place holder
+    retval = "0.0"
+
+    # If we are building from a branch using Travis-CI, append the build number so we know where the
+    # package came from
+    if 'TRAVIS_BUILD_NUMBER' in os.environ:
+        retval += "." + os.environ['TRAVIS_BUILD_NUMBER']
+    else:
+        retval += ".0"
+
+    # Pre release versions need a non-numeric suffix on the version number
+    retval += ".dev0"
+
+    return retval
+
 
 # Dynamically generate a list of sub-modules to be packaged with the project
 # Always exclude unit test modules from the package
@@ -7,16 +54,20 @@ _packages = find_packages(exclude=['tests', 'tests.*'])
 
 setup(
     name='friendlyshell',
-    version='0.0.1',
+    version=get_version_number(),
     author='Kevin S. Phillips',
-    author_email='thefriendlycoder@gmail.com',
+    author_email='kevin@thefriendlycoder.com',
     packages=_packages,
-    description='Replacement for "cmd" built-in library with enhanced functionality',
-    url='',
+    description="Framework for writing interactive Python command line interfaces, similar to the 'cmd' build in class.",
+    long_description=open('README.rst').read(),
+    url='https://github.com/TheFriendlyCoder/friendlyshell',
     install_requires=['pyparsing', 'tabulate'],
+    keywords='cmd command shell interactive interpreter',
+    license="GPL",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Console",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Natural Language :: English",
         "Operating System :: MacOS",
         "Operating System :: Microsoft",
@@ -24,6 +75,9 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5"
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Software Development :: Libraries"
     ]
 )
